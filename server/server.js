ef6ca338f6747ce397cf65019878397767b01e47
Andrea Ramirez <andrea.anza3@gmail.com>
2023-06-06 02:00:28 -0500

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+}
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../React-Portfolio/dist/index.html"));
 });
