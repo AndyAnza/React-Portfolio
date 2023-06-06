96a5eaa030488c84065e6817cb780ab46df1c281
Andrea Ramirez <andrea.anza3@gmail.com>
2023-06-05 21:59:20 -0500

    test

--- React-Portfolio/src/components/Header.jsx
+++ React-Portfolio/src/components/Header.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { PhoneIcon, PlayCircleIcon } from "@heroicons/react/20/solid";
 
 const callsToAction = [
   { name: "Watch demo", href: "#", icon: PlayCircleIcon },
