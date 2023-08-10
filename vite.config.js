19d4307e424ef0e7d578fa106196c355c73ee342
Andrea Ramirez <andrea.anza3@gmail.com>
2023-08-10 17:53:46 -0600

    Fixing files

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "/React-Portfolio/dist", // Specify the output directory as "dist"
+    outDir: "dist", // Specify the output directory as "dist"
   },
 });
