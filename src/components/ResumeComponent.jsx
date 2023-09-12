c0cfe9af322ef1867843ed28b94ca290e3583349
Andrea Ramirez <andrea.anza3@gmail.com>
2023-09-11 23:32:49 -0600

    Fixed resume page, added download button

--- src/components/ResumeComponent.jsx
+++ src/components/ResumeComponent.jsx
@@ -1,10 +1,31 @@
-import React from "react";
-import resumePDF from "../assets/AndreaAnzaResume.pdf";
+// import resumePDF from "../assets/AndreaAnzaResume.pdf";
+import "../css/aboutMe.css";
 
 function ResumeComponent() {
   return (
-    <div className="flex justify-center">
-      <object data={resumePDF} width="1600" height="800"></object>
+    // <div className="flex justify-center">
+    //   <object data={resumePDF} width="1600" height="800"></object>
+    // </div>
+    <div>
+      <div className="flex justify-center py-6">
+        <iframe
+          src="https://drive.google.com/file/d/1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i/preview"
+          width="70%"
+          height="700"
+        ></iframe>
+      </div>
+      <div className="flex justify-center">
+        <a
+          data-text="Awesome"
+          className="button"
+          href="https://drive.google.com/uc?export=1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i"
+        >
+          <span className="actual-text">&nbsp;Download&nbsp;</span>
+          <span className="hover-text" aria-hidden="true">
+            &nbsp;Download&nbsp;
+          </span>
+        </a>
+      </div>
     </div>
   );
 }
