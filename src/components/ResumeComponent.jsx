1de40e20804bcbce27b9fd4ff510a08d9b611f24
Andrea Ramirez <andrea.anza3@gmail.com>
2023-09-12 00:25:12 -0600

    Added Toast to resume page

--- src/components/ResumeComponent.jsx
+++ src/components/ResumeComponent.jsx
@@ -1,7 +1,9 @@
 // import resumePDF from "../assets/AndreaAnzaResume.pdf";
 import "../css/aboutMe.css";
+import { ToastContainer, useToast } from "@rewind-ui/core";
 
 function ResumeComponent() {
+  const toast = useToast();
   return (
     // <div className="flex justify-center">
     //   <object data={resumePDF} width="1600" height="800"></object>
@@ -18,13 +20,32 @@ function ResumeComponent() {
         <a
           data-text="Awesome"
           className="button"
-          href="https://drive.google.com/uc?export=1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i"
+          href="https://drive.google.com/uc?export=download&id=1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i"
+          download="AndreaAnzaResume.pdf"
+          onClick={() => {
+            toast.add({
+              id: "unique-id",
+              closeOnClick: true,
+              color: "green",
+              description: "",
+              duration: 2000,
+              iconType: "success",
+              pauseOnHover: true,
+              radius: "md",
+              shadow: "none",
+              shadowColor: "green",
+              showProgress: true,
+              title: "Download complete!",
+              tone: "solid",
+            });
+          }}
         >
           <span className="actual-text">&nbsp;Download&nbsp;</span>
           <span className="hover-text" aria-hidden="true">
             &nbsp;Download&nbsp;
           </span>
         </a>
+        <ToastContainer position="bottom-right" />
       </div>
     </div>
   );
