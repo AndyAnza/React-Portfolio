2650a6259f088c07af8814ee31fd3b97e24d61a9
Andrea Ramirez <andrea.anza3@gmail.com>
2024-05-12 23:10:26 -0600

    Updated resume url

--- src/components/ResumeComponent.jsx
+++ src/components/ResumeComponent.jsx
@@ -1,9 +1,9 @@
-import "../css/greenButton.css";
+import '../css/greenButton.css';
 
-import "../css/loader.css";
-import "../css/resume.css";
-import { ToastContainer, useToast } from "@rewind-ui/core";
-import { useState } from "react";
+import '../css/loader.css';
+import '../css/resume.css';
+import { ToastContainer, useToast } from '@rewind-ui/core';
+import { useState } from 'react';
 
 function ResumeComponent() {
   const toast = useToast();
@@ -11,63 +11,63 @@ function ResumeComponent() {
 
   return (
     <div>
-      <div className="container-margin flex justify-center">
+      <div className='container-margin flex justify-center'>
         <a
-          data-text="Awesome"
-          className="greenButton"
-          href="https://drive.google.com/uc?export=download&id=1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i"
-          download="AndreaAnzaResume.pdf"
+          data-text='Awesome'
+          className='greenButton'
+          href='https://drive.google.com/uc?export=download&id=1ISv5B0ZBYQNAa6vVjr52ESHVqg4-W492'
+          download='AndreaAnzaResume.pdf'
           onClick={() => {
             toast.add({
-              id: "unique-id",
+              id: 'unique-id',
               closeOnClick: true,
-              color: "green",
-              description: "",
+              color: 'green',
+              description: '',
               duration: 2000,
-              iconType: "success",
+              iconType: 'success',
               pauseOnHover: true,
-              radius: "md",
-              shadow: "none",
-              shadowColor: "green",
+              radius: 'md',
+              shadow: 'none',
+              shadowColor: 'green',
               showProgress: true,
-              title: "Success!",
-              tone: "solid",
+              title: 'Success!',
+              tone: 'solid',
             });
           }}
         >
-          <span className="actual-text">&nbsp;Download&nbsp;</span>
-          <span className="hover-text" aria-hidden="true">
+          <span className='actual-text'>&nbsp;Download&nbsp;</span>
+          <span className='hover-text' aria-hidden='true'>
             &nbsp;Download&nbsp;
           </span>
         </a>
-        <ToastContainer position="bottom-right" className="toaster" />
+        <ToastContainer position='bottom-right' className='toaster' />
       </div>
       {!iframeLoaded ? (
-        <div className="loader-container">
-          <div className="loader">
-            <div className="cell d-0"></div>
-            <div className="cell d-1"></div>
-            <div className="cell d-2"></div>
+        <div className='loader-container'>
+          <div className='loader'>
+            <div className='cell d-0'></div>
+            <div className='cell d-1'></div>
+            <div className='cell d-2'></div>
 
-            <div className="cell d-1"></div>
-            <div className="cell d-2"></div>
+            <div className='cell d-1'></div>
+            <div className='cell d-2'></div>
 
-            <div className="cell d-2"></div>
-            <div className="cell d-3"></div>
+            <div className='cell d-2'></div>
+            <div className='cell d-3'></div>
 
-            <div className="cell d-3"></div>
-            <div className="cell d-4"></div>
+            <div className='cell d-3'></div>
+            <div className='cell d-4'></div>
           </div>
         </div>
       ) : null}
 
-      <div className="resume-container">
+      <div className='resume-container'>
         <iframe
-          src="https://drive.google.com/file/d/1XRB9Y1AZgoXQgVRrHEuvurnnMX2_4i7i/preview"
-          width="70%"
-          height="700"
+          src='https://drive.google.com/file/d/1ISv5B0ZBYQNAa6vVjr52ESHVqg4-W492/preview'
+          width='70%'
+          height='700'
           onLoad={() => setIframeLoaded(true)}
-          className={`${iframeLoaded ? "square-in-hesitate" : "hidden"}`}
+          className={`${iframeLoaded ? 'square-in-hesitate' : 'hidden'}`}
         ></iframe>
       </div>
     </div>
