8f58a53366834578fc36971070fe69856e0dd11b
Andrea Ramirez <andrea.anza3@gmail.com>
2025-04-19 19:56:57 -0600

    Fix: minmax range and sm width

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,14 +1,14 @@
 // import '../css/skills.css';
 export default function Skills({ description, skills }) {
   return (
-    <div className='tech-main-container m-6 w-[1000px] '>
+    <div className='tech-main-container sm: w-[90vw] m-6 lg: w-[50vw] '>
       <h2
         className='lg: bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-transparent text-4xl text-center m-4'
         id='portfolio'
       >
         {description}
       </h2>
-      <div className='tech-container grid gap-4 grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))]  '>
+      <div className='tech-container grid gap-4 grid-cols-[repeat(auto-fit,_minmax(150px,_1fr))]  '>
         {skills.map(skill => (
           <div
             key={skill.name}
